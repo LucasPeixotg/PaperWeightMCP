bcfef711cd75cbdbf16f100970316d8cd38f62f8
Lucas Peixoto <Lucaspeixotopro@gmail.com>
2026-09-06 17:40:25 -0300

    fixed naming issue

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,4 +1,4 @@
 # tools/__init__.py
-from .find_relevant_papers.tool import find_revelant_papers
+from .find_relevant_papers.tool import find_relevant_papers
 
-__all__ = ["find_revelant_papers"]
+__all__ = ["find_relevant_papers"]
